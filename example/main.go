e8fbddb92683eaa6985b7f81bb41a8317a0b8cae
54mch4n <54mch4n.1111@gmail.com>
2020-05-15 13:41:34 +0900

    upgrade: major specification changes

--- example/main.go
+++ example/main.go
@@ -19,7 +19,7 @@ func main() {
 	if sl, err := dup.Int(); err == nil {
 		fmt.Printf("extracted slice -> %#v\n", sl)
 	}
-	fmt.Printf("original  slice -> %#v / No change.\n\n", sliceInt)
+	fmt.Printf("original  slice -> %#v // No change.\n\n", sliceInt)
 
 	fmt.Println("Case: []float64")
 	sliceFloat64 := []float64{0.1, 0.1, 0.2, 0.2, 0.3, 0.3}
@@ -27,7 +27,12 @@ func main() {
 	if sl, err := dup.Float64(); err == nil {
 		fmt.Printf("extracted slice -> %#v\n", sl)
 	}
-	fmt.Printf("original  slice -> %#v / No change.\n\n", sliceFloat64)
+	fmt.Printf("original  slice -> %#v // No change.\n\n", sliceFloat64)
+
+	fmt.Println("Case: []bool")
+	sliceBool := []bool{true, true, true, true, true, true, true, true, true, true, true, true, false, false, false}
+	dedupe.Do(&sliceBool)
+	fmt.Printf("original  slice -> %#v\n\n", sliceBool)
 
 	type duplication struct {
 		Name string
@@ -50,7 +55,7 @@ func main() {
 		},
 	}
 	dedupe.Do(&sliceStruct)
-	fmt.Printf("original  slice -> %#v\n\n", sliceStruct)
+	fmt.Printf("original  slice -> %#v\n", sliceStruct)
 
 	sliceStruct = []duplication{
 		{
@@ -67,10 +72,10 @@ func main() {
 		},
 	}
 	dup.Do(&sliceStruct)
-	if sl, err := dup.Struct(); err == nil {
+	if sl, err := dup.Any(); err == nil {
 		fmt.Printf("extracted slice -> %#v\n", sl)
 	}
-	fmt.Printf("original  slice -> %#v / No change.\n\n", sliceStruct)
+	fmt.Printf("original  slice -> %#v // No change.\n\n", sliceStruct)
 
 	fmt.Println("Case: []Pointer")
 	slicePtr := []*duplication{
@@ -88,7 +93,7 @@ func main() {
 		},
 	}
 	dedupe.Do(&slicePtr)
-	fmt.Printf("original  slice -> %#v\n\n", slicePtr)
+	fmt.Printf("original  slice -> %#v\n", slicePtr)
 
 	slicePtr = []*duplication{
 		{
@@ -105,15 +110,50 @@ func main() {
 		},
 	}
 	dup.Do(&slicePtr)
-	if sl, err := dup.Struct(); err == nil {
+	if sl, err := dup.Any(); err == nil {
 		fmt.Printf("extracted slice -> %#v\n", sl)
 	}
-	fmt.Printf("original  slice -> %#v / No change.\n\n", slicePtr)
+	fmt.Printf("original  slice -> %#v // No change.\n\n", slicePtr)
 
-	// TODO FIXME
 	fmt.Println("Case: []UniqueType")
 	type AgeInt int
 	sliceUnique := []AgeInt{1, 2, 3, 3}
-	fmt.Println(dedupe.Do(&sliceUnique))
+	dedupe.Do(&sliceUnique)
 	fmt.Printf("original  slice -> %#v\n\n", sliceUnique)
+
+	fmt.Println("Case: []UniqueType2")
+	type otherType duplication
+	sliceOtherType := []otherType{
+		{
+			Name: "A",
+			Age:  1,
+		},
+		{
+			Name: "A",
+			Age:  1,
+		},
+		{
+			Name: "B",
+			Age:  2,
+		},
+	}
+	dedupe.Do(&sliceOtherType)
+	fmt.Printf("original  slice -> %#v\n\n", sliceOtherType)
+
+	fmt.Println("Case: []func")
+	type Func func(string) bool
+	sliceFunc := []Func{
+		func(s string) bool {
+			return true
+		},
+		func(s string) bool {
+			return false
+		},
+		func(s string) bool {
+			return true
+		},
+	}
+	if err := dedupe.Do(&sliceFunc); err != nil {
+		fmt.Println(err)
+	}
 }
