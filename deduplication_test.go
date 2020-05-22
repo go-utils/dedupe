aecc4c88b232c00bba678d28b0464bdcdbc2e3e9
54mch4n <54mch4n.1111@gmail.com>
2020-05-22 22:29:22 +0900

    tests: add more case

--- deduplication_test.go
+++ deduplication_test.go
@@ -7,9 +7,241 @@ import (
 	"testing"
 
 	"github.com/go-utils/dedupe"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 )
+
+func TestDo(t *testing.T) {
+	t.Run("Case:[]string", func(tr *testing.T) {
+		expected := []string{"Go", "V", "Java", "Python", "Ruby"}
+		slice := []string{"Go", "V", "Java", "Python", "Go", "Ruby", "Go", "V"}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	dup := dedupe.NewDeduplication()
+
+	t.Run("Case:[]int", func(tr *testing.T) {
+		slice := []int{1, 1, 2, 2, 3, 3}
+		dup.Do(&slice)
+		if sl, err := dup.Int(); err == nil {
+			expected := []int{1, 2, 3}
+			if diff := cmp.Diff(sl, expected); diff != "" {
+				tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+			}
+		}
+		if diff := cmp.Diff(slice, slice); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]float64", func(tr *testing.T) {
+		slice := []float64{0.1, 0.1, 0.2, 0.2, 0.3, 0.3}
+		dup.Do(&slice)
+		if sl, err := dup.Float64(); err == nil {
+			expected := []float64{0.1, 0.2, 0.3}
+			if diff := cmp.Diff(sl, expected); diff != "" {
+				tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+			}
+		}
+		if diff := cmp.Diff(slice, slice); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]bool", func(tr *testing.T) {
+		expected := []bool{true, false}
+		slice := []bool{true, true, true, true, true, true, true, true, true, true, true, true, false, false, false}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	type duplication struct {
+		Name string
+		Age  int
+	}
+
+	t.Run("Case:[]struct", func(tr *testing.T) {
+		expected := []duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+
+		slice := []duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+
+		slice = []duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		dup.Do(&slice)
+		if sl, err := dup.Any(); err == nil {
+			if diff := cmp.Diff(sl, expected); diff != "" {
+				tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+			}
+		}
+
+		if diff := cmp.Diff(slice, slice); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]Pointer", func(tr *testing.T) {
+		expected := []*duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+
+		slice := []*duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+
+		slice = []*duplication{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		dup.Do(&slice)
+		if sl, err := dup.Any(); err == nil {
+			if diff := cmp.Diff(sl, expected); diff != "" {
+				tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+			}
+		}
+
+		if diff := cmp.Diff(slice, slice); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]UniqueType", func(tr *testing.T) {
+		type Int int
+		expected := []Int{1, 2, 3}
+		slice := []Int{1, 2, 3, 3}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]UniqueType2", func(tr *testing.T) {
+		type otherType duplication
+		expected := []otherType{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		slice := []otherType{
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "A",
+				Age:  1,
+			},
+			{
+				Name: "B",
+				Age:  2,
+			},
+		}
+		dedupe.Do(&slice)
+		if diff := cmp.Diff(slice, expected); diff != "" {
+			tr.Errorf("%s: unexpected, differs: (-got +want)\n%s", tr.Name(), diff)
+		}
+	})
+
+	t.Run("Case:[]func", func(tr *testing.T) {
+		type Func func(string) bool
+		slice := []Func{
+			func(s string) bool {
+				return true
+			},
+			func(s string) bool {
+				return false
+			},
+			func(s string) bool {
+				return true
+			},
+		}
+		if err := dedupe.Do(&slice); err == nil {
+			tr.Fatal("not supported")
+		}
+	})
+}
+
+// ================== BenchMark ==================
 
 func deDuplicate1(args []int) []int {
 	results := make([]int, 0, len(args))
